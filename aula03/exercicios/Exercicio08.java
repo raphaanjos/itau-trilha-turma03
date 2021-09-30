b538f64c0309ca76d082b1c7ecfce89caa9cb98c
Raphael dos Anjos <raphaanjos1@gmail.com>
2021-09-30 09:55:39 -0300

    aula 03 - exercício08.

--- aula03/exercicios/Exercicio08.java
+++ aula03/exercicios/Exercicio08.java
@@ -7,6 +7,28 @@ public class Exercicio08 {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
+        double ladoA, ladoB, ladoC;
+
+        System.out.println("Insira um valor: ");
+        ladoA = entrada.nextDouble();
+
+        System.out.println("Insira o segundo valor: ");
+        ladoB = entrada.nextDouble();
+
+        System.out.println("Insira o terceiro valor: ");
+        ladoC = entrada.nextDouble();
+
+        if (ladoA > (ladoB + ladoC) || ladoB > (ladoA + ladoC) || ladoC > (ladoA + ladoB)) {
+            System.out.println("As medias informadas não formam triângulo algum.");
+        }else {
+            if (ladoA == ladoB && ladoA == ladoC && ladoC == ladoB) {
+                System.out.println("As medias informadas formam um triângulo equilátero.");
+            }else if (ladoA == ladoB || ladoA == ladoC || ladoC == ladoB) {
+                System.out.println("As medias informadas formam um triângulo isósceles.");
+            } else {
+                System.out.println("As medias informadas formam um triângulo escaleno.");
+            }
+        }
 
         entrada.close();
     }
